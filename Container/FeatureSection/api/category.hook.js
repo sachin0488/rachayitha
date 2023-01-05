561ee899eccb6db9f6d8a0affbe3af2d42b74266
Ayush Kumar Singh <54079897+developerAkX@users.noreply.github.com>
2023-01-05 15:50:41 +0530

    Delete Container directory

--- Container/FeatureSection/api/category.hook.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import { categoryApi } from './category.api'
-
-const useCategoryApi = () => {
-  const { data, isLoading, isError, error, isFetching } = useQuery(['use-category'], categoryApi)
-  return { data, isLoading, isError, error, isFetching }
-}
-
-export default useCategoryApi
