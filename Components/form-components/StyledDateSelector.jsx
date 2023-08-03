d15d5d3c73f9cedb1b12d0a9d6d2f5761f998345
developerAkX <ayushks11560@gmail.com>
2023-08-03 23:13:23 +0530

    Updated Library

--- Components/form-components/StyledDateSelector.jsx
+++ Components/form-components/StyledDateSelector.jsx
@@ -1,11 +1,10 @@
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers'
 import { useController, useFormContext } from 'react-hook-form'
-import { StyledTextFieldRoot } from './StyledTextField'
+import styled from '@emotion/styled'
 
-const StyledDateSelector = ({ name, label, rules, required, sx }) => {
-  const formContext = useFormContext()
-  const { control, errors } = formContext
+const StyledDateSelector = ({ name, label, rules, required, ...props }) => {
+  const { control, errors } = useFormContext()
 
   const { field } = useController({
     name,
@@ -21,30 +20,61 @@ const StyledDateSelector = ({ name, label, rules, required, sx }) => {
   const isError = Boolean(errorMessage)
 
   const rootProps = {
+    size: 'medium',
     error: isError ? isError : undefined,
     label: label,
     required: required || Boolean(rules?.required),
     disableFuture: true,
     value: field.value,
-    openTo: 'year',
-    // ampm: false,
-    views: ['year', 'month', 'day'],
-    onBlur: field.onBlur,
     name: field.fieldName,
     inputRef: field.ref,
+    onBlur: field.onBlur,
     onChange: field.onChange,
-    inputFormat: 'DD/MM/YYYY',
+    format: 'DD/MM/YYYY',
   }
 
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
-      <DatePicker
-        {...rootProps}
-        // disableOpenPicker
-        renderInput={params => <StyledTextFieldRoot {...params} sx={sx} />}
-      />
+      <StyledDatePicker {...rootProps} slotProps={{ textField: { size: 'medium' } }} {...props} />
     </LocalizationProvider>
   )
 }
+
+const StyledDatePicker = styled(DatePicker)`
+  .MuiInputBase-input {
+    box-shadow: black !important;
+  }
+
+  .MuiFormLabel-root {
+    font-size: 0.94rem;
+    line-height: 1.245;
+  }
+
+  .MuiFormLabel-asterisk {
+    font-size: 0.84rem;
+    line-height: 1;
+  }
+
+  .MuiOutlinedInput-root {
+    font-size: 0.81rem;
+  }
+
+  .MuiOutlinedInput-input {
+    padding: 8.5px 14px;
+    -webkit-autofill {
+      box-shadow: none;
+      -webkit-text-fill-color: none;
+    }
+  }
+
+  .MuiOutlinedInput-notchedOutline {
+    border-color: ${({ theme }) => theme.palette.text.tertiary}93;
+    border-width: 1.5px;
+  }
+
+  .MuiSvgIcon-root {
+    color: ${({ theme }) => theme.palette.text.icon};
+  }
+`
 
 export default StyledDateSelector
