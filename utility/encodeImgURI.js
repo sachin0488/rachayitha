180bde3ee2b053f0cb4cbe2556c8b0f4b091d57c
Ayush Kumar Singh <ayushks11560@gmail.com>
2024-07-25 23:41:58 +0530

    fxd

--- utility/encodeImgURI.js
+++ utility/encodeImgURI.js
@@ -2,13 +2,11 @@ const encodeImgURI = uri => {
   let arr = uri.split('/')
 
   for (let i = 0; i < arr.length; i++) {
-    if(arr.length - 2 <= i) {
+    if (arr.length - 2 <= i) {
       arr[i] = encodeURIComponent(arr[i])
-      console.log(arr[i])
       break
     }
   }
-
 
   return arr.join('/')
 }
