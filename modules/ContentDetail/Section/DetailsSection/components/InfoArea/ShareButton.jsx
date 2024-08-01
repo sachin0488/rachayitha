f3dfd24b55d00a33fca046926534d24ce2b1a715
amanak7 <22je0094@iitism.ac.in>
2024-08-02 02:16:01 +0530

    remove unwanted changes

--- modules/ContentDetail/Section/DetailsSection/components/InfoArea/ShareButton.jsx
+++ modules/ContentDetail/Section/DetailsSection/components/InfoArea/ShareButton.jsx
@@ -1,42 +1,46 @@
 import { Button } from '@mui/material'
 import ShareRoundedIcon from '@mui/icons-material/ShareRounded'
+import { useRouter } from 'next/router'
 
-const ShareButton = ({coverImage}) => {
+const ShareButton = ({ coverImage }) => {
+  const { asPath } = useRouter()
+
   const handleClick = async () => {
     try {
-      const fileUrl = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fletsenhance.io%2F&psig=AOvVaw21FVLBsNiwEsEnZZXbQN8p&ust=1722547249464000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLCttNma0ocDFQAAAAAdAAAAABAH'
+      let invitationLink = `${window.location.origin}${asPath}`
 
-      // Fetch the file content from the URL
-      const response = await fetch({fileUrl})
+      const response = await fetch({
+        method: 'GET',
+        url: coverImage,
+      })
+
       const blob = await response.blob()
+      const file = new File([blob], 'image.jpg', { type: 'image/jpeg' })
 
-      // Create a File object from the blob
-      const file = new File([blob], 'shared-file.png', { type: blob.type })
-
-      // Check if the Web Share API supports file sharing
-      if (navigator.canShare && navigator.canShare({ files: [file] })) {
-        await navigator.share({
-          title: 'Check out this file!',
-          text: 'I’m sharing this file with you.',
-          files: [file],
-        })
-      } else {
-        console.warn('Web Share API with files is not supported on this browser')
-      }
+      navigator.share({
+        title: document.title,
+        text: 'Check out this amazing content!',
+        url: invitationLink,
+        files: [file],
+      })
     } catch (error) {
       console.error('Error sharing:', error)
     }
   }
 
   return (
-    <Button
-      variant="text"
-      onClick={handleClick}
-      sx={{ minWidth: 36, width: 36 }}
-      disableElevation
-    >
-      <ShareRoundedIcon />
-    </Button>
+    <>
+      <Button
+        variant="text"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        sx={{ minWidth: 36, width: 36 }}
+        disableElevation>
+        <ShareRoundedIcon />
+      </Button>
+    </>
   )
 }
 
